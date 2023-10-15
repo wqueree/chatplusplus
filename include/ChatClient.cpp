4f3ed24fb03315c16d365cdb4f9df47f0d2d27c9
Will Queree <wqueree@gmail.com>
2023-10-15 21:56:53 +0100

    Added ChatClient class

---
+++ include/ChatClient.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wqueree on 15/10/23.
+//
+
+#include "ChatClient.h"
---
+++ include/ChatClient.h
@@ -0,0 +1,14 @@
+//
+// Created by wqueree on 15/10/23.
+//
+
+#ifndef CHATPLUSPLUS_CHATCLIENT_H
+#define CHATPLUSPLUS_CHATCLIENT_H
+
+
+class ChatClient {
+
+};
+
+
+#endif //CHATPLUSPLUS_CHATCLIENT_H
